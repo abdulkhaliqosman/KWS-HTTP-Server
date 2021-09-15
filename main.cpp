3affade04554ff0938e51f2774ab840a7cb226f2
Abdul Khaliq Bin Osman <khaliqand_92@hotmail.com>
2021-09-15 09:55:06 +0800

    Multithreading support
    Made a thread pool that will wait until a connection is assigned

--- main.cpp
+++ main.cpp
@@ -7,13 +7,121 @@
 #include <cstdio>
 
 #include "file.cpp"
+#include <pthread.h>
 
 constexpr int BACKLOG_COUNT = 5;
 constexpr int MAXLINE = 2048;
 constexpr int DEFAULT_PORT = 8080;
+#include <sys/sysinfo.h>
+
+#include <fcntl.h>
+
+void *SocketThread(void *arg); // forward declaration
+
+struct ThreadInfo
+{
+    pthread_t m_ThreadId = 0;
+    pthread_mutex_t mutex;
+    pthread_mutex_t mutex2;
+    pthread_cond_t cond;
+    int m_ThreadNum = 0;
+    int m_ConnFd = 0;
+    bool m_Waiting = false;
+
+    void init(const pthread_attr_t &attr)
+    {
+        pthread_mutex_init(&mutex, nullptr);
+        pthread_mutex_init(&mutex2, nullptr);
+        pthread_cond_init(&cond, nullptr);
+        pthread_create(&m_ThreadId, &attr, &SocketThread, (void *)this);
+    }
+
+    void destroy()
+    {
+        pthread_join(m_ThreadId, nullptr);
+        pthread_mutex_destroy(&mutex);
+        pthread_mutex_destroy(&mutex2);
+        pthread_cond_destroy(&cond);
+    }
+};
+
+void *SocketThread(void *arg)
+{
+    ThreadInfo *threadInfo = (ThreadInfo *)arg;
+    while (true)
+    {
+        pthread_mutex_lock(&threadInfo->mutex);
+        pthread_mutex_lock(&threadInfo->mutex2);
+
+        threadInfo->m_Waiting = true;
+        pthread_mutex_unlock(&threadInfo->mutex);
+
+        pthread_cond_wait(&threadInfo->cond, &threadInfo->mutex2);
+        pthread_mutex_unlock(&threadInfo->mutex2);
+
+        // printf("Thread %d running, wait = %s\n", threadInfo->m_ThreadNum, threadInfo->m_Waiting ? "true" : "false");
+        char method[8];
+        char uri[MAXLINE];
+        char httpver[16];
+
+        {
+            char recvline[MAXLINE + 1];
+            int recvlen = recv(threadInfo->m_ConnFd, recvline, MAXLINE, 0);
+            if (recvlen == -1)
+            {
+                perror("recv error");
+                exit(1);
+            }
+
+            recvline[recvlen] = 0;
+
+            int num = sscanf(recvline, "%7s %2047s %15s", method, uri, httpver);
+
+            if (num < 2)
+            {
+                method[0] = 0;
+            }
+            else
+            {
+                // printf("recvmsg=%s\n", recvline);
+                // printf("recvlen = %d\n", recvlen);
+
+                // printf("HTTP request method: %s\n", method);
+                // printf("uri: %s\n", uri);
+            }
+        }
+
+        // We do not support anything other than GET /
+        if (strcmp(method, "GET") == 0 && strcmp(uri, "/") == 0)
+        {
+            char sendline[MAXLINE];
+
+            char filebuf[MAXLINE];
+
+            const char *response = "HTTP/1.1 200 OK";
+            const char *contentType = "Content-Type: text/html";
+            const char *contentLength = "Content-Length";
+
+            int filelen = readfile("./index.html", filebuf, MAXLINE);
+            //! How you make sure sendline have enought space ?
+            int sendlen = snprintf(sendline, MAXLINE, "%s\n%s\n%s: %d\n\n%s", response, contentType, contentLength, filelen, filebuf);
+
+            // printf("sendmsg: %s\n", sendline);
+            // printf("sendlen =%d\n", sendlen);
+
+            int sentlen = send(threadInfo->m_ConnFd, sendline, sendlen, 0);
+            // printf("sent = %d\n", sentlen);
+        }
+
+        close(threadInfo->m_ConnFd);
+    }
+    return nullptr;
+}
 
 int main(int argc, char *argv[])
 {
+    int num_procs = get_nprocs();
+
     printf("Welcome to Khaliq Web Services kwserver!\n");
     fflush(stdout);
 
@@ -29,6 +137,9 @@ int main(int argc, char *argv[])
         perror("ERROR opening socket");
         exit(1);
     }
+
+    int flags = fcntl(socketfd, F_GETFL, 0);
+    fcntl(socketfd, F_SETFL, flags | O_NONBLOCK);
 
     bzero((char *)&serv_addr, sizeof(serv_addr));
 
@@ -56,6 +167,19 @@ int main(int argc, char *argv[])
 
     int maxfdp1 = socketfd + 1;
 
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    printf("Number of processors: %d\n", num_procs);
+
+    int num_threads = num_procs - 1;
+    ThreadInfo *threadPool = new ThreadInfo[num_threads];
+
+    for (int i = 0; i < num_threads; ++i)
+    {
+        threadPool[i].m_ThreadNum = i + 1;
+        threadPool[i].init(attr);
+    }
+
     while (true)
     {
         int readyFD = select(maxfdp1, &rset, nullptr, nullptr, nullptr);
@@ -65,64 +189,54 @@ int main(int argc, char *argv[])
             exit(1);
         }
 
-        printf("readyFD = %d\n", readyFD);
         if (FD_ISSET(socketfd, &rset))
         { /* socket is readable */
+            // printf("Socket is readable\n");
+            // find first sleeping thread
 
-            int connfd = accept(socketfd, (sockaddr *)&cli_addr, &cli_addr_len);
-            char method[8];
-            char uri[MAXLINE];
-            char httpver[16];
-
+            // bool read = false;
+            for (int i = 0; i < num_threads; ++i)
             {
-                char recvline[MAXLINE+1];
-                int recvlen = recv(connfd, recvline, MAXLINE, 0);
-                if (recvlen == -1)
+                ThreadInfo &thread = threadPool[i];
+                pthread_mutex_lock(&thread.mutex);
+
+                if (thread.m_Waiting)
                 {
-                    perror("recv error");
-                    exit(1);
+                    // printf("thread i = %d is ready\n", i + 1);
+                    sockaddr_in cli_addr;
+                    socklen_t cli_addr_len = sizeof(cli_addr);
+
+                    int connfd = accept(socketfd, (sockaddr *)&cli_addr, &cli_addr_len);
+                    if (connfd >= 0)
+                    {
+                        thread.m_ConnFd = connfd;
+                        thread.m_Waiting = false;
+
+                        pthread_cond_signal(&thread.cond);
+                    }
+                    else if(errno == EWOULDBLOCK || errno == EAGAIN)
+                    {
+                        break;
+                    }
+
+                    pthread_mutex_unlock(&thread.mutex);
+                    break;
                 }
-
-                recvline[recvlen] = 0;
-
-                int num = sscanf(recvline, "%7s %2047s %15s", method, uri, httpver);
-                
-                if( num < 2 ) {
-                    method[0] = 0;
-                }else {
-
-                    printf("recvmsg=%s\n", recvline);
-                    printf("recvlen = %d\n", recvlen);
-
-                    printf("HTTP request method: %s\n", method);
-                    printf("uri: %s\n", uri);
+                else
+                {
+                    pthread_mutex_unlock(&thread.mutex);
                 }
             }
-
-            // We do not support anything other than GET /
-            if (strcmp(method, "GET") == 0 && strcmp(uri, "/") == 0)
-            {
-                char sendline[MAXLINE];
-
-                char filebuf[MAXLINE];
-
-                const char *response = "HTTP/1.1 200 OK";
-                const char *contentType = "Content-Type: text/html";
-                const char *contentLength = "Content-Length";
-
-                int filelen = readfile("./index.html", filebuf, MAXLINE);
-                //! How you make sure sendline have enought space ?
-                int sendlen = snprintf(sendline, MAXLINE, "%s\n%s\n%s: %d\n\n%s", response, contentType, contentLength, filelen, filebuf);
-
-                printf("sendmsg: %s\n", sendline);
-                printf("sendlen =%d\n", sendlen);
-
-                int sentlen = send(connfd, sendline, sendlen, 0);
-                printf("sent = %d\n", sentlen);
-            }
-
-            close(connfd);
         }
     }
+
+    for (int i = 0; i < num_threads; ++i)
+    {
+        threadPool[i].destroy();
+    }
+
+    delete[] threadPool;
+
+    pthread_attr_destroy(&attr);
     close(socketfd);
 }
